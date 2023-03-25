cb3a108274fb0b04ff69e5152ef00bd420bc7366
Siltrim <rojerr410@gmail.com>
2023-03-25 17:18:10 +0600

    First commit

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import debounce from 'lodash.debounce';
+import styles from './Search.module.scss';
+import { useDispatch } from 'react-redux';
+import { setSearchValue } from '../../redux/slices/filterSlice';
+
+export const Search: React.FC = () => {
+  const dispatch = useDispatch();
+  const [value, setValue] = React.useState('');
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const onClickClear = () => {
+    dispatch(setSearchValue(''));
+    setValue('');
+    inputRef.current?.focus();
+  };
+
+  // eslint-disable-next-line
+  const updateSearchValue = React.useCallback(
+    debounce((str: string) => {
+      dispatch(setSearchValue(str));
+    }, 150),
+    [],
+  );
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
+
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        fill="none"
+        height="24"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg">
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" x2="16.65" y1="21" y2="16.65" />
+      </svg>
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+
+      {value && (
+        <svg
+          onClick={onClickClear}
+          className={styles.clearIcon}
+          viewBox="0 0 48 48"
+          xmlns="http://www.w3.org/2000/svg">
+          <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z" />
+          <path d="M0 0h48v48h-48z" fill="none" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NotFoudnBlock from '../components/NotFoundBlock';
+
+export const NotFound: React.FC = () => {
+  return <NotFoudnBlock />;
+};
+
+export default NotFound;
